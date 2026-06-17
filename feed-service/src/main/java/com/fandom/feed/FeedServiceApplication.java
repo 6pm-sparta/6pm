c5efb8ea71bcf8414727c35f449edcc5b5fc3ea8
gray-ji <seeu314@gmail.com>
2026-06-17 11:53:20 +0900

    feat: 게시글 생성 API 구현
    Refs #13

--- feed-service/src/main/java/com/fandom/feed/FeedServiceApplication.java
+++ feed-service/src/main/java/com/fandom/feed/FeedServiceApplication.java
@@ -1,13 +1,11 @@
-package com.fandom.feed_service;
+package com.fandom.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FeedServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FeedServiceApplication.class, args);
 	}
-
 }
---
+++ feed-service/src/main/java/com/fandom/feed/presentation/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.fandom.feed.presentation.controller;
+
+import com.fandom.common.auth.UserIdCard;
+import com.fandom.common.auth.annotation.CurrentIdCard;
+import com.fandom.common.dto.ApiResponse;
+import com.fandom.feed.application.service.PostService;
+import com.fandom.feed.presentation.dto.request.PostRequest;
+import com.fandom.feed.presentation.dto.response.PostResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping("/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<PostResponse.Create> create(
+            @Valid PostRequest request,
+            @CurrentIdCard UserIdCard idCard
+    ) {
+        PostResponse.Create response = postService.create(request.content(), request.imageKeys(), idCard.getUserId());
+        return ApiResponse.created(response);
+    }
+}
