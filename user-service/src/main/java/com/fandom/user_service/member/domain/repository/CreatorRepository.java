60624ae3dca183fc4ac56f916d914f8c82a2b456
Jaebeom Lee <126840713+iPad7@users.noreply.github.com>
2026-06-18 02:09:32 +0900

    Revert "[feat] 회원 도메인 및 회원가입/내부조회 API 구현 (#19)"
    This reverts commit 77a248f519bf37962c4a0929c87932241f527972.

--- user-service/src/main/java/com/fandom/user_service/member/domain/repository/CreatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fandom.user_service.member.domain.repository;
-
-import com.fandom.user_service.member.domain.entity.Creator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CreatorRepository extends JpaRepository<Creator, UUID> {
-}
