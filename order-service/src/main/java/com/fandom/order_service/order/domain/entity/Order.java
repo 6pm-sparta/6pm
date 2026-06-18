9e2ca43649f4c343d113df843a72fbd4f81d5ace
김승현 <shkim7020@gmail.com>
2026-06-19 00:50:13 +0900

    refactor: jakarta.persistence import 정리
    Refs #52

--- order-service/src/main/java/com/fandom/order_service/order/domain/entity/Order.java
+++ order-service/src/main/java/com/fandom/order_service/order/domain/entity/Order.java
@@ -1,7 +1,13 @@
 package com.fandom.order_service.order.domain.entity;
 
 import com.fandom.common.entity.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
