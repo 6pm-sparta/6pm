bde474c600d6679b56efdd21eed3f200c5b5438e
look516 <happycatcindy@gmail.com>
2026-06-24 13:34:18 +0900

    [chore] import 정리

--- ticketing-service/src/main/java/com/fandom/ticketing_service/seat/controller/SeatController.java
+++ ticketing-service/src/main/java/com/fandom/ticketing_service/seat/controller/SeatController.java
@@ -9,7 +9,12 @@ import com.fandom.ticketing_service.seat.dto.ShowSeatResponse;
 import com.fandom.ticketing_service.seat.service.SeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
