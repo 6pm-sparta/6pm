23cb4153c004cf27704be449d8f3d6e1b2dab16e
Jaebeom Lee <8000gam@gmail.com>
2026-06-24 12:29:30 +0900

    fix: 크리에이터 간 팔로우 허용
    Ref #147

--- user-service/src/main/java/com/fandom/user_service/follow/domain/exception/FollowErrorCode.java
+++ user-service/src/main/java/com/fandom/user_service/follow/domain/exception/FollowErrorCode.java
@@ -12,7 +12,8 @@ public enum FollowErrorCode implements ErrorCode {
     FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "팔로우 관계를 찾을 수 없습니다."),
     DUPLICATE_FOLLOW(HttpStatus.CONFLICT, "이미 팔로우한 크리에이터입니다."),
     SELF_FOLLOW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "자기 자신은 팔로우할 수 없습니다."),
-    FOLLOWER_MUST_BE_MEMBER(HttpStatus.FORBIDDEN, "일반회원만 팔로우할 수 있습니다."),
+    FOLLOWER_MUST_BE_MEMBER(HttpStatus.FORBIDDEN, "일반 회원만 팔로우할 수 있습니다."),
+    FOLLOWER_MUST_BE_MEMBER_OR_CREATOR(HttpStatus.FORBIDDEN, "일반 회원 또는 크리에이터만 팔로우할 수 있습니다."),
     FOLLOWEE_MUST_BE_CREATOR(HttpStatus.BAD_REQUEST, "크리에이터만 팔로우할 수 있습니다.");
 
     private final HttpStatus status;
