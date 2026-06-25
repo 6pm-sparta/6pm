de6d14c6a280675bd5b16791fc1c7c0b5cb6dfe2
조아영 <80755497+look516@users.noreply.github.com>
2026-06-25 14:07:18 +0900

    [docs] 티켓팅 문서 정리 (#159)
    ## 관련 이슈
    Closes #153
    
    ## 변경 유형
    - [X] docs
    
    ## 관련 서비스/모듈
    ticketing
    
    ## 변경 내용
    - ticketing-system.md 가 main입니다.

--- ticketing-service/src/main/java/com/fandom/ticketing_service/queue/controller/QueueController.java
+++ ticketing-service/src/main/java/com/fandom/ticketing_service/queue/controller/QueueController.java
@@ -9,7 +9,11 @@ import com.fandom.ticketing_service.queue.service.QueueSseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
