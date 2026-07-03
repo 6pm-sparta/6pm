75ca6155b6904949bc8a4968a55ef2ba5b0fc30d
gray-ji <seeu314@gmail.com>
2026-07-03 20:43:13 +0900

    fix: 이미지 이름/키 검증 정규식 오류 수정

--- feed-service/src/main/java/com/fandom/feed/presentation/dto/request/validation/ImageKeyValidator.java
+++ feed-service/src/main/java/com/fandom/feed/presentation/dto/request/validation/ImageKeyValidator.java
@@ -4,7 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ImageKeyValidator implements ConstraintValidator<ValidImageKey, String> {
-    private static final String REGEX = "^posts/\\d{8}/[0-9a-fA-F-]{36}\\.[a-zA-Z]{3,4}$";
+    private static final String REGEX = "^posts/\\d{8}/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\\.[a-zA-Z]{3,4}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
