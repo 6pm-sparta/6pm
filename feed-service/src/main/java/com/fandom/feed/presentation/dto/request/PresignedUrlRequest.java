4c327c4e3aae47d15e2d979aa78b31054b777848
gray-ji <seeu314@gmail.com>
2026-07-03 20:44:31 +0900

    feat: 이미지 개수 제한 어노테이션 추가

--- feed-service/src/main/java/com/fandom/feed/presentation/dto/request/PresignedUrlRequest.java
+++ feed-service/src/main/java/com/fandom/feed/presentation/dto/request/PresignedUrlRequest.java
@@ -1,13 +1,13 @@
 package com.fandom.feed.presentation.dto.request;
 
+import com.fandom.feed.presentation.dto.request.validation.ValidImageLimit;
 import com.fandom.feed.presentation.dto.request.validation.ValidImageName;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record PresignedUrlRequest(
         @NotEmpty(message = "이미지를 선택해주세요.")
-        @Size(max = 4, message = "이미지는 최대 4개까지 가능합니다.")
+        @ValidImageLimit
         List<@ValidImageName String> imageNames
 ) {}
