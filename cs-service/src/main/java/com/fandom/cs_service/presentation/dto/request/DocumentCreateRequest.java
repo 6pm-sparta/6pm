b1be76ee68d42841e2e81c50cf723d4448c99684
tlsdud135 <kytbible@gmail.com>
2026-07-05 03:06:12 +0900

    feat: dto 추가 Refs #267

---
+++ cs-service/src/main/java/com/fandom/cs_service/presentation/dto/request/DocumentCreateRequest.java
@@ -0,0 +1,11 @@
+package com.fandom.cs_service.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DocumentCreateRequest(
+        @NotBlank(message = "문서 제목을 입력해주세요.")
+        String title,
+        @NotBlank(message = "문서 내용을 입력해주세요.")
+        String content
+) {
+}
